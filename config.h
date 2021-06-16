27f6529a957e8deb723ff7e6fe15656517741ef0
camilo <camilo@cvv>
2021-06-15 22:58:34 -0400

    act

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 /* appearance */
 static const unsigned int borderpx = 2;   /* border pixel of windows */
 static const unsigned int snap     = 32;  /* snap pixel */
-static const unsigned int gappx    = 6;   /* pixel gap between clients */
+static const unsigned int gappx    = 8;   /* pixel gap between clients */
 static const int showbar           = 1;   /* 0 means no bar */
 static const int topbar            = 1;   /* 0 means bottom bar */
 static const int horizpadbar       = 6;   /* horizontal padding for statusbar */
